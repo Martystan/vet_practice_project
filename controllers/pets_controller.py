301db8c637400428b3ae2140203387439c9b84b4
Martyna Stanislawska <martynastan@gmail.com>
2021-10-03 12:15:59 +0100

    changes to controllers and templates

--- controllers/pets_controller.py
+++ controllers/pets_controller.py
@@ -3,3 +3,6 @@ from flask import Blueprint
 from models.pet import Pet
 import repositories.pet_repository as pet_repository
 import repositories.vet_repository as vet_repository
+
+pets_blueprint = Blueprint("pets", __name__)
+
