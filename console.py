9f7c01f7b3f3d6d1aa744624966f178837753433
Martyna Stanislawska <martynastan@gmail.com>
2021-10-05 15:18:55 +0100

    more css

--- console.py
+++ console.py
@@ -19,6 +19,20 @@ pet_repository.save(pet1)
 pet2 = Pet("Crookshanks","https://s36537.pcdn.co/wp-content/uploads/2018/11/persian-cat-face.jpg.optimal.jpg", "01/09/2010", "Persian", "Hermione Granger", "012345667788", "hermione@hogwarts.com", "some notes", vet2 )
 pet_repository.save(pet2)
 
+pet3 = Pet("Marley", "http://media2.s-nbcnews.com/i/msnbc/Components/Photos/060123/060123_marleyandme_vmed_230p.jpg", "1/01/2010", "Labrador Retriever", "John Grogan", "098765432", "john@gmail.com", "Destructive but cute", vet2 )
+pet_repository.save(pet3)
+
+pet4 = Pet("Marley", "http://media2.s-nbcnews.com/i/msnbc/Components/Photos/060123/060123_marleyandme_vmed_230p.jpg", "1/01/2010", "Labrador Retriever", "John Grogan", "098765432", "john@gmail.com", "Destructive but cute", vet2 )
+pet_repository.save(pet4)
+
+pet5 = Pet("Marley", "http://media2.s-nbcnews.com/i/msnbc/Components/Photos/060123/060123_marleyandme_vmed_230p.jpg", "1/01/2010", "Labrador Retriever", "John Grogan", "098765432", "john@gmail.com", "Destructive but cute", vet2 )
+pet_repository.save(pet5)
+
+pet6 = Pet("Marley", "http://media2.s-nbcnews.com/i/msnbc/Components/Photos/060123/060123_marleyandme_vmed_230p.jpg", "1/01/2010", "Labrador Retriever", "John Grogan", "098765432", "john@gmail.com", "Destructive but cute", vet2 )
+pet_repository.save(pet6)
+
+
+
 # vet1.name = "Dr Dolittle"
 # vet_repository.update(vet1)
 
